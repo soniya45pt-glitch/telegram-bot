3406f20efd25a0e99b1817d42afbe4e1108c2111
soniya45pt-glitch <soniya45pt@gmail.com>
2026-04-11 02:28:36 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,12 @@
+import time
 
+users = {}
+
+def add_user(user_id):
+    users[user_id] = {"paid": False}
+
+def set_paid(user_id):
+    users[user_id]["paid"] = True
+
+def is_paid(user_id):
+    return users.get(user_id, {}).get("paid", False)
